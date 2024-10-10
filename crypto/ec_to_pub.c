f30fcb8a4d90c7b02dd01b35c693f2578bebd5a0
Pablo <p5.tacoronte@gmail.com>
2024-10-10 15:59:25 +0000

    fixing form

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -13,6 +13,7 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 	char *buf = NULL;
 	const EC_GROUP *g = NULL;
 	const EC_POINT *p = NULL;
+	point_conversion_form_t f;
 
 	if (!key)
 		return (NULL);
@@ -20,7 +21,8 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 	p = EC_KEY_get0_public_key(key);
 	if (!g || !p)
 		return (NULL);
-	buf = EC_POINT_point2hex(g, p, POINT_CONVERSION_UNCOMPRESSED, NULL);
+	f = EC_KEY_get_conv_form(key);
+	buf = EC_POINT_point2hex(g, p, f, NULL);
 	for (; i < EC_PUB_LEN; i++)
 		pub[i] = buf[i];
 	OPENSSL_free(buf);
