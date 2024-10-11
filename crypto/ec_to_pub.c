77ddb5ffd8ba0e94588487237c78d946eb0bdd74
Pablo <p5.tacoronte@gmail.com>
2024-10-11 04:31:41 -0700

    Revert "fixed public key returned format"
    This reverts commit f3ed8a38da00d7c84b9935277f2cda936320de4c.

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -9,16 +9,22 @@
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	unsigned char *buffer = NULL;
-	size_t len = 0, i = 0;
+	int i = 0 ;
+	char *buf = NULL;
+	const EC_GROUP *g = NULL;
+	const EC_POINT *p = NULL;
 	point_conversion_form_t f;
 
 	if (!key)
 		return (NULL);
+	g = EC_KEY_get0_group(key);
+	p = EC_KEY_get0_public_key(key);
+	if (!g || !p)
+		return (NULL);
 	f = EC_KEY_get_conv_form(key);
-	len = EC_KEY_key2buf(key, f, &buffer, NULL);
-	for (; i < len; i++)
-		pub[i] = buffer[i];
-	OPENSSL_free(buffer);
+	buf = EC_POINT_point2hex(g, p, f, NULL);
+	for (; i < EC_PUB_LEN; i++)
+		pub[i] = buf[i];
+	OPENSSL_free(buf);
 	return (pub);
 }
