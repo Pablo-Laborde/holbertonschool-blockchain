b984f346207bd88d0e800ba8b06f77df74335bd5
Pablo <p5.tacoronte@gmail.com>
2024-10-11 04:37:37 -0700

    fixes

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -8,8 +8,7 @@
  * @digest: uint8_t *
  * Return: uint8_t *
  */
-uint8_t *sha256(int8_t const *s, size_t len,
-				uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!digest)
 		return (NULL);
