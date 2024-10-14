e4f032bf6550e0fceb5060136af08f4ff58d0d1b
Pablo <p5.tacoronte@gmail.com>
2024-10-13 19:08:23 -0700

    Fix border case

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -11,7 +11,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	EC_KEY *key = NULL;
 
 	if (!pub)
-		return (ec_from_pub_errors(3));
+		return (ec_from_pub_errors(0));
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 		return (ec_from_pub_errors(1));
@@ -33,7 +33,5 @@ void *ec_from_pub_errors(int error)
 		printf("Key is NULL.\n");
 	if (error == 2)
 		printf("Failed to set public key.\n");
-	if (error == 3)
-		printf("PUB is NULL.\n");
 	return (NULL);
 }
