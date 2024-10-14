f56f4873f6a40867aa7406ba2ed6c63d8b8150e3
Pablo <p5.tacoronte@gmail.com>
2024-10-13 18:56:03 -0700

    Creating key from public key

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,35 @@
+#include "hblk_crypto.h"
+void _print_hex_buffer(uint8_t const *buf, size_t len);
+#include <openssl/err.h>
+/**
+ * ec_from_pub- func
+ * @pub: uint8_t const *
+ * Return: EC_KEY *
+ */
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	EC_KEY *key = NULL;
+
+	key = ec_create();
+	if (!key)
+		return (ec_from_pub_errors(1));
+	if (!EC_KEY_oct2key(key, (unsigned char *)pub, EC_PUB_LEN, NULL))
+		return (ec_from_pub_errors(2));
+	EC_KEY_set_private_key(key, NULL);
+	return (key);
+}
+
+
+/**
+ * ec_from_pub_errors- func
+ * @error: int
+ * Return: void *
+ */
+void *ec_from_pub_errors(int error)
+{
+	if (error == 1)
+		printf("Key is NULL.\n");
+	if (error == 2)
+		printf("Failed to create key.\n");
+	return (NULL);
+}
