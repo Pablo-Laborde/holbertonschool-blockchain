589f7cb826f59d3e7c9de9928936cc4cdffb793f
Pablo Laborde <p5.tacoronte@gmail.com>
2024-11-07 09:23:31 -0300

    little fixes

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -12,13 +12,10 @@
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 				sig_t const *sig)
 {
-	int len = (int)sig->len;
-	EC_KEY *k = NULL;
-
 	if (!key || !msg || !sig)
 		return (0);
-	k = (EC_KEY *)key;
-	if (!ECDSA_verify(0, msg, msglen, (unsigned char *)sig->sig, len, k))
+	if (ECDSA_verify(0, msg, msglen, (unsigned char *)sig->sig,
+			sig->len, (EC_KEY *)key) != 1)
 		return (0);
 	return (1);
 }
