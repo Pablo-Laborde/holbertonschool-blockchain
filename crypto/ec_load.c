b8111207468e8cbf20693da27e607a4ac040428c
Pablo Laborde <p5.tacoronte@gmail.com>
2024-11-12 17:01:16 -0300

    rebuilding path instead of using chdir

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -8,15 +8,24 @@
  */
 EC_KEY *ec_load(char const *folder)
 {
+	int len = 0;
+	char *path = NULL;
 	FILE *fd = NULL;
 	EC_KEY *key = NULL;
 
-	if (!folder || (chdir(folder) == -1))
+	if (!folder)
+		return (NULL);
+	len = strlen(folder) + 13;
+	path = malloc(sizeof(char) * len);
+	if (!path)
 		return (NULL);
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 		return (NULL);
 	fd = fopen(PUB_FILENAME, "r");
+
+	if (!sprintf(path, "%s/%s", folder, PUB_FILENAME))
+		return (NULL);
 	if (!fd || !PEM_read_EC_PUBKEY(fd, &key, NULL, NULL))
 	{
 		EC_KEY_free(key);
@@ -24,11 +33,14 @@ EC_KEY *ec_load(char const *folder)
 	}
 	fclose(fd);
 	fd = fopen(PRI_FILENAME, "r");
+	if (!sprintf(path, "%s/%s", folder, PRI_FILENAME))
+		return (NULL);
 	if (!fd || !PEM_read_ECPrivateKey(fd, &key, NULL, NULL))
 	{
 		EC_KEY_free(key);
 		return (NULL);
 	}
 	fclose(fd);
+	free(path);
 	return (key);
 }
