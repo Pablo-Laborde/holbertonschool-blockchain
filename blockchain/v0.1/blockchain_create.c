89472114ab42e0eb992c3fc63d1d926dcf70cd89
Pablo Laborde <p5.tacoronte@gmail.com>
2024-11-24 13:52:31 -0300

    cpy all hash

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -34,11 +34,12 @@ blockchain_t *blockchain_create(void)
 	memset(block->data.buffer, 0, BLOCKCHAIN_DATA_MAX);
 	strcpy((char *)&block->data.buffer, "Holberton School");
 	block->data.len = strlen((char *)block->data.buffer);
-	strcpy((char *)block->hash,
-					"\xc5\x2c\x26\xc8\xb5\x46\x16\x39"
-					"\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
-					"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1"
-					"\xd3\xd7\xff\x2f\x04\x51\x58\x03");
+	memcpy((char *)block->hash,
+			"\xc5\x2c\x26\xc8\xb5\x46\x16\x39"
+			"\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
+			"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1"
+			"\xd3\xd7\xff\x2f\x04\x51\x58\x03",
+			SHA256_DIGEST_LENGTH);
 		/* c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803 */
 	llist_add_node(blockchain->chain, block, ADD_NODE_FRONT);
 	return (blockchain);
