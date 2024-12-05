1460557e19b64be3f9cc106164b60f226cd1963d
Pablo Laborde <p5.tacoronte@gmail.com>
2024-12-05 20:00:25 -0300

    hashing once before comparing to difficulty

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -8,10 +8,12 @@
  */
 void block_mine(block_t *block)
 {
-	if (block)
-		while (!hash_matches_difficulty(block->hash, block->info.difficulty))
-		{
-			block->info.nonce++;
-			block_hash(block, block->hash);
-		}
+	if (!block)
+		return ();
+	block_hash(block, block->hash);
+	while (!hash_matches_difficulty(block->hash, block->info.difficulty))
+	{
+		block->info.nonce++;
+		block_hash(block, block->hash);
+	}
 }
